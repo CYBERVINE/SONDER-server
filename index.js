6a73ee513d9b438ae6688c716a6bece23c9f58c5
Kevin Byrne <kevinspbyrne@gmail.com>
2023-12-23 16:33:48 -0800

    initialize server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+      
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
