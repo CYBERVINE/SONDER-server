5fa7608a77633fa72c0e8039de64a977ba3414f8
Kevin Byrne <kevinspbyrne@gmail.com>
2023-12-24 15:18:19 -0800

    make db table lat lng precise

--- seeds/posts.js
+++ seeds/posts.js
@@ -3,10 +3,9 @@ const postsData = require('../seed-data/posts');
 
 
 exports.seed = async function(knex) {
-  console.log(postsData[9].lng)
   await knex('posts').del();
   await knex('users').del();
   await knex('users').insert(usersData);
-  await knex('posts').insert(postsData); // code precision here
+  await knex('posts').insert(postsData); 
 };
 
